2bdf86a6c0a095ea950edbef0eef8a472918b575
Eugene Shuster <eugeneshuster@Eugenes-MacBook-Pro.local>
2020-07-27 10:36:17 -0700

    quick hotfix to responses

--- shared/responses.py
+++ shared/responses.py
@@ -9,8 +9,8 @@ class Responses(Response):
 
     # CREATED
     @classmethod
-    def status_201(self):
-        return Response(status=status.HTTP_201_CREATED, data=None)
+    def status_201(self, data=None):
+        return Response(status=status.HTTP_201_CREATED, data=data)
 
     # BAD_REQUEST
     @classmethod
@@ -20,29 +20,29 @@ class Responses(Response):
     # UNAUTHORIZED
     @classmethod
     def status_401(self, data=None): # Lacks valid authenitcation credentials
-        return Response(status=status.HTTP_401_UNAUTHORIZED, data=None)
+        return Response(status=status.HTTP_401_UNAUTHORIZED, data=data)
 
     # FORBIDDEN
     @classmethod
     def status_403(self, data=None): # Has credentials but is not allowed
-        return Response(status=status.HTTP_403_FORBIDDEN, data=None)
+        return Response(status=status.HTTP_403_FORBIDDEN, data=data)
 
     # NOT FOUND
     @classmethod
     def status_404(self, data=None):
-        return Response(status=status.HTTP_404_NOT_FOUND, data=None)
+        return Response(status=status.HTTP_404_NOT_FOUND, data=data)
 
     # INTERNAL SERVER ERROR
     @classmethod
     def status_500(self, data=None):
-        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data=None)
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data=data)
 
     # BAD_GATEWAY (Gateway receives invalid response from origin server, Domain name unresolvable)
     @classmethod
     def status_502(self, data=None):
-        return Response(status=status.HTTP_502_BAD_GATEWAY, data=None)
+        return Response(status=status.HTTP_502_BAD_GATEWAY, data=data)
 
     # SERVICE UNAVAILABLE
     @classmethod
     def status_503(self, data=None):
-        return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE, data=None)
+        return Response(status=status.HTTP_503_SERVICE_UNAVAILABLE, data=data)
